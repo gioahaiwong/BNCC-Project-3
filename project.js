6ce9518b2c2cfeefaa67280c27e424006dd6a780
gioahaiwong <giovanwongso@gmail.com>
2025-02-24 20:38:13 +0700

    Add files via upload

--- project.js
+++ project.js
@@ -1,3 +1,18 @@
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href");
+  const targetSection = document.querySelector(targetId);
+
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+}
+
+const navbarLinks = document.querySelectorAll(".navbar-link");
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
 const button = document.querySelector(".button-fun");
 
 button.addEventListener("click", () => {
